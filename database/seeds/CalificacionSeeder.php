528a969272593e0f8518fb5f63f00427b933bcaf
Stefanny Aguirre <sbaguirr@espol.edu.ec>
2019-09-05 10:08:16 -0500

    Seed

--- database/seeds/CalificacionSeeder.php
+++ database/seeds/CalificacionSeeder.php
@@ -21,7 +21,7 @@ class CalificacionSeeder extends Seeder
         ]);
         Calificacion::create([
             'comentario' => 'Es fantástico llegar con dudas a clase que para tí son irresolubles y que Vicent en unos pocos minutos te deje todo los conceptos claros y con la sensación de que todo es más fácil de como viene en los libros. Es un crack. Me encanta como explica',
-            'valoracion' => 5,
+            'valoracion' => 4,
             'id_estudiante' => 'E032587419',
             'id_tutor' => 'T034578101',
         ]);
@@ -33,7 +33,7 @@ class CalificacionSeeder extends Seeder
         ]);
         Calificacion::create([
             'comentario' => 'Bien, explica muy bien.',
-            'valoracion' => 5,
+            'valoracion' => 4,
             'id_estudiante' => 'E920135782',
             'id_tutor' => 'T098784562',
         ]);
@@ -45,7 +45,7 @@ class CalificacionSeeder extends Seeder
         ]);
         Calificacion::create([
             'comentario' => 'Excelente en todos los sentidos, tanto en explicación como en comprensión de las dudas, se nota que domina de sobra el temario. Explica todo de una forma muy sencilla que ayuda a retener los conceptos. Recomiendo 100%!',
-            'valoracion' => 5,
+            'valoracion' => 4,
             'id_estudiante' => 'E013256985',
             'id_tutor' => 'T175433246',
         ]);
